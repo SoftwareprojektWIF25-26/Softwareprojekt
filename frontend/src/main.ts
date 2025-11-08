0de1cd4f76de6f134b1c8cadc6e692eebbb5cd6f
Denise <denise.bohne@st.ovgu.de>
2025-11-08 14:42:41 +0100

    Schriftart Barlow hinzugefügt

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,7 +1,8 @@
 import './assets/main.css'
-
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+import './assets/Schriftarten/Schriftart.css'
+
 
 import App from './App.vue'
 import router from './router'
