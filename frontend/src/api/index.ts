52bfacb22865208078301d78ff396d478fd3506a
Denise <denise.bohne@st.ovgu.de>
2025-11-09 13:37:08 +0100

    Update api

--- frontend/src/api/index.ts
+++ frontend/src/api/index.ts
@@ -3,5 +3,19 @@ function getProjektListe(): Promise<{
 }> {
   return request({
     url: '/api/projektliste',
+    method: 'get',
   })
+}
+
+
+function getProjektById(id: number): Promise<{ projekt: Projekt }> {
+  return request({
+    url: `/api/projekte/${id}`,
+    method: 'get',
+  })
+}
+
+export default {
+  getProjektListe,
+  getProjektById
 }
