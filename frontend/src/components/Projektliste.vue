a549612a3b0f22345dec6a46fb7b7121749fd209
Denise <denise.bohne@st.ovgu.de>
2025-12-29 12:04:49 +0100

    Update Projektliste #5

--- frontend/src/components/Projektliste.vue
+++ frontend/src/components/Projektliste.vue
@@ -1,17 +1,24 @@
 <script setup lang="ts">
-import {onMounted} from "vue";
-import {ref} from "vue";
+import {onMounted, watch} from "vue";
 import {useRouter} from "vue-router";
 import api from "@/api";
+import {Projekt} from "@/types";
 
-
-const projects = ref([])
+const status =ref("")
+const projects = ref<Projekt[]>([]);
 const router = useRouter()
 
+async function getfilteredProjekte(){
+  const res = await api.getZuletztBearbeitet({status: status.value})
+  projects.value = res.data ?? [];
+}
+
+watch(status,()=>{getfilteredProjekte()})
 function goToDashboard(id) {
   router.push({ name: 'dashboard', params: { id } })
 
 }
+
 onMounted(async () => {
   const project = await api.getProjektListe()
   projects.value = project.data
@@ -20,15 +27,12 @@ onMounted(async () => {
 
 <template>
   <section class="w-full max-w-6xl p-8 mx-auto">
-    <!-- Grauer Kasten um Überschrift + Projektliste -->
     <div class="grauer-kasten">
 
-      <!-- Überschrift -->
       <h1>
         Meine Projekte
       </h1>
 
-      <!-- Projektliste -->
       <div v-if="projects.length === 0"
            class="flex items-center justify-center min-h-[200px]">
         <span >
@@ -38,13 +42,26 @@ onMounted(async () => {
 
       <div v-else
            class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
+        <div>
+          <select v-model="status">
+            <option value="">Alle</option>
+            <option value="PLANNING">In Planung</option>
+            <option value="IN_PROGRESS">In Bearbeitung</option>
+            <option value="COMPLETED">Abgeschlossen</option>
+            <option value="CANCELLED">Abgebrochen</option>
+            <option value="ON-HOLD">Pausiert</option>
+
+          </select>
+        </div>
         <div
           v-for="project in projects"
-          :key="project.id"
+          :key="project.ID"
           class="p-6 bg-white rounded-2xl shadow-md hover:shadow-xl transition cursor-pointer"
-          @click="goToDashboard(project.id)">
-          <h2>{{ project.name }}</h2>
-        </div>
+          @click="goToDashboard(project.ID)">
+          <h2>{{ project.Titel}}</h2>
+          <p> {{project.Teamgroesse}}</p>
+          <p>{{project.Status}}</p>
+          </div>
       </div>
 
     </div>
